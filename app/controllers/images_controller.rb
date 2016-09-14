4d8c78bda6f8c2361796564a757855cb0d5b29b2
Alvin Ng and Leah Nelson <alvin+leah@>
2016-09-14 15:44:35 -0700

    add CRUD functionality only if current user is the user who uploaded the photos

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,25 +1,41 @@
 class ImagesController < ApplicationController
-  def new
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
     @user = User.find(params[:user_id])
+    @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new
+    @user = current_user
     @image = @user.images.new
+    if @user.id != current_user.id
+      redirect_to user_path(@user)
+    end
   end
 
   def create
-    @user = User.find(params[:user_id])
+    @user = current_user
     @image = @user.images.new(image_params)
-    if @image.save
-      flash[:success] = "Image uploaded!"
-      redirect_to user_path(@user)
-    else
-      flash[:alert] = "Upload unsuccessful, please try again!"
-      render :new
+      if @image.save
+        flash[:success] = "Image uploaded!"
+        redirect_to user_path(@user)
+      else
+        flash[:alert] = "Upload unsuccessful, please try again!"
+        render :new
     end
   end
 
   def destroy
     @image = Image.find(params[:id])
-    @image.destroy
-    flash[:error] = "Image deleted!"
+    if @image.user_id === current_user.id
+      @image.destroy
+      flash[:error] = "Image deleted!"
+    end
     redirect_to user_path(@image.user)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root :to => "application#index"
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   resources :users do
-    resources :images
+    resources :images, only: [:index]
   end
+  resources :images, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
